06d84b7404a284296a84c608185b86549cf9b30e
eduar <eduar@DESKTOP-SV6GQVL>
2023-03-13 23:00:02 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public interface IAnimal
+{
+    void MakeSound();
+}
+
+public class Dog : IAnimal
+{
+    public void MakeSound()
+    {
+        Console.WriteLine("Woof!");
+    }
+}
+
+public class Cat : IAnimal
+{
+    public void MakeSound()
+    {
+        Console.WriteLine("Meow!");
+    }
+}
+
+public class Program
+{
+    public static void AnimalSound(IAnimal animal)
+    {
+        animal.MakeSound();
+    }
+
+    public static void Main()
+    {
+        IAnimal dog = new Dog();
+        IAnimal cat = new Cat();
+
+        AnimalSound(dog); // Woof!
+        AnimalSound(cat); // Meow!
+    }
+}
